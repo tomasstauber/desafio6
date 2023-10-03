621f56bb9119aefed296c0ff930f2c65cbe83a4c
Tomás Stauber <tomystauber@gmail.com>
2023-10-03 09:02:06 -0300

    fix desafio6

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -41,10 +41,6 @@ router.get("/login", async (req, res) => {
     res.render("login");
 });
 
-router.get("/logout", async (req, res) => {
-    res.redirect("/");
-})
-
 router.get("/register", async (req, res) => {
     res.render("register");
 });
